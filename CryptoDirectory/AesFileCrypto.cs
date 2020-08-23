b5bb9db8bc9f5dd17a797383150913825b8d8269
Aiden <47725379+Mewrin@users.noreply.github.com>
2020-08-23 20:52:02 +0100

    Add cryptography methods
    Need to somehow be able to decrypt a file from a previous aes encryption which used a different key/iv

---
+++ CryptoDirectory/AesFileCrypto.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoDirectory
+{
+    public static class AesFileCrypto
+    {
+        private static Encoding _encoding = new UTF8Encoding();
+
+        public static byte[] Encrypt(string plainText, byte[] key, byte[] iv)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("key");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException("iv");
+            
+            byte[] encryptedData;
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+                aesAlg.Padding = PaddingMode.Zeros;
+
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encryptedData = msEncrypt.ToArray();    
+                    }
+                }
+            }
+
+            return encryptedData;
+        }
+
+        public static string Decrypt(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("key");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException("iv");
+
+            string plaintext;
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+                aesAlg.Padding = PaddingMode.Zeros;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }    
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
---
+++ CryptoDirectory/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoDirectory
+{
+    class Program
+    {
+        private static string _filePath = "/Users/aidenwilde/Documents/Development/CryptoDirectory/CryptoDirectory/test.txt";
+        
+        static void Main(string[] args)
+        {
+            var encoding = new UTF8Encoding();
+            var filedata = File.ReadAllText(_filePath);            
+            using (AesCryptoServiceProvider myAes = new AesCryptoServiceProvider())
+            {
+                byte[] encrypted = AesFileCrypto.Encrypt(filedata, myAes.Key, myAes.IV);
+                string decrypted = AesFileCrypto.Decrypt(encrypted, myAes.Key, myAes.IV);
+
+                Console.WriteLine("encrypted: {0}", encoding.GetString(encrypted));
+                Console.WriteLine("decrypted: {0}", decrypted);
+            }
+        }
+    }
+}
